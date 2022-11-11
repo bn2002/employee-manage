b818410c25437406ee668c3cf5e1583ede2d1143
BN2002 <doanhcms@gmail.com>
2022-11-11 12:48:51 +0700

    Add create employee

--- api/src/main/java/com/bn2002/cukcuk/api/services/EmployeeService.java
+++ api/src/main/java/com/bn2002/cukcuk/api/services/EmployeeService.java
@@ -1,23 +1,81 @@
 package com.bn2002.cukcuk.api.services;
 
+import com.bn2002.cukcuk.api.dtos.EmployeeDto;
+import com.bn2002.cukcuk.api.models.Department;
 import com.bn2002.cukcuk.api.models.Employee;
+import com.bn2002.cukcuk.api.models.Position;
+import com.bn2002.cukcuk.api.repositories.DepartmentRepository;
 import com.bn2002.cukcuk.api.repositories.EmployeeRepository;
+import com.bn2002.cukcuk.api.repositories.PositionRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import javax.persistence.EntityExistsException;
+import javax.swing.text.html.Option;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService {
     @Autowired
-    private EmployeeRepository repository;
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private DepartmentRepository departmentRepository;
+    @Autowired
+    private PositionRepository positionRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
     public List<Employee> getAllEmployees() {
-        return repository.findAll();
+        return employeeRepository.findAll();
     }
 
-    public Employee createNewEmployee(Employee employee) {
-        return new Employee();
+    public Employee createNewEmployee(EmployeeDto employee) {
+        // Kiểm tra email tồn tại hay chưa
+        Optional<Employee> isEmailExist =  employeeRepository.getEmployeesByEmail(employee.getEmail());
+        if(isEmailExist.isPresent()) {
+            throw  new EntityExistsException("Email này đã tồn tại, hãy kiểm tra lại");
+        }
+
+        //Kiểm tra mã nhân viên tồn tại hay chưa
+        Optional<Employee> isEmployeeCodeExist =  employeeRepository.getEmployeesByEmployeeCode(employee.getEmployeeCode());
+        if(isEmployeeCodeExist.isPresent()) {
+            throw  new EntityExistsException("Mã nhân viên này đã tồn tại, hãy kiểm tra lại");
+        }
+        // Kiểm tra số CMND/CCCD tồn tại chưa
+        Optional<Employee> isIdentityNumberExist =  employeeRepository.getEmployeesByIdentityNumber(employee.getIdentityNumber());
+        if(isIdentityNumberExist.isPresent()) {
+            throw  new EntityExistsException("Số CMND/CCCD này đã tồn tại, hãy kiểm tra lại");
+        }
+        // Lấy thông tin vị trí và phòng ban
+        Optional<Position> position = positionRepository.getPositionById(employee.getPositionId());
+        if(!position.isPresent()) {
+            throw new NoSuchElementException("Vị trí này không tồn tại");
+        }
+
+        Optional<Department> department = departmentRepository.getDepartmentById(employee.getDepartmentId());
+        if(!department.isPresent()) {
+            throw new NoSuchElementException("Phòng ban này không tồn tại");
+        }
+        // Set 1 số giá trị theo ID mà người dùng cung cấp
+        employee.setDepartmentName(department.get().getDepartmentName());
+        employee.setPostionName(position.get().getPositionName());
+        employee.setCreatedBy("Nguyen Duy Doanh");
+        // Map Dto qua Entity
+        Employee newEmployee = modelMapper.map(employee, Employee.class);
+        System.out.println(newEmployee.toString());
+        employeeRepository.save(newEmployee);
+        return newEmployee;
     }
 
     public boolean checkExists(String employeeCode) {
@@ -25,7 +83,7 @@ public class EmployeeService {
     }
 
     public String getLastestEmployeeCode() {
-        String currentEmployeeCode = repository.getMaxEmployeeCode();
+        String currentEmployeeCode = employeeRepository.getMaxEmployeeCode();
         if(currentEmployeeCode.length() < 1) {
             return "NV00001";
         }
