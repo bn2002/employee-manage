1c36b9282ed8c3f451f2ebf398a942e0adb41d1a
BN2002 <doanhcms@gmail.com>
2022-11-11 13:50:15 +0700

    Update Employee mapping

--- api/src/main/java/com/bn2002/cukcuk/api/configs/ModelMapperConfig.java
+++ api/src/main/java/com/bn2002/cukcuk/api/configs/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package com.bn2002.cukcuk.api.configs;
 
+import com.bn2002.cukcuk.api.dtos.EmployeeDto;
+import com.bn2002.cukcuk.api.models.Employee;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +13,9 @@ public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STANDARD);
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT).setAmbiguityIgnored(true);
+        modelMapper.createTypeMap(Employee.class, EmployeeDto.class)
+                .addMapping(Employee::getEmployeeID, EmployeeDto::setId);
         return modelMapper;
     }
 }
